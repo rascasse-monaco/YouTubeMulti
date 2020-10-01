a2676749b0112abbbd3bb014940baac376662897
rascasse-monaco <mktwkhr@gmail.com>
2020-10-02 02:27:04 +0900

    動画の大きさを設定するプルダウン追加

--- add-form.js
+++ add-form.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let urlNum = 1 ;//動画URLのナンバー
+let resolution = 'width="480" height="270"';
 
 function addForm() {
 
@@ -26,7 +27,7 @@ function addForm() {
     addButton.type = 'button';
     addButton.id = 'embed_button';
     addButton.value = '動画を埋め込み';
-    addButton.setAttribute("onClick", "embed(urlNum)");
+    addButton.setAttribute("onClick", "embed(urlNum);resoSet()");
 
   embedButton.appendChild(addButton);
 
@@ -45,7 +46,7 @@ function embed(){
 
   var input_url = document.getElementById("input_url-" + urlNum).value;
   var input_url = input_url.split('v=')[1];//URLから動画のIDを抽出
-  var iframe = '<iframe width="480" height="270" src="https://www.youtube.com/embed/' + input_url + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';//iframeに動画のIDを代入
+  var iframe = '<iframe ' + resolution + ' src="https://www.youtube.com/embed/' + input_url + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';//iframeに動画のIDを代入
   //埋め込みエリアに動画（iframe）を埋め込み
   var output_url = document.getElementById("output_url-" + urlNum);
   output_url.innerHTML = iframe;
@@ -69,4 +70,15 @@ function embed(){
   var removeEmbedBotton = document.getElementById('form_button_area');
   var embedButton = document.getElementById('embed_button');
   removeEmbedBotton.removeChild(embedButton);
+
+  //動画サイズプルダウンメニュー削除（初回埋め込み移行エラー出るので要修正）
+  var removeResoMenu = document.getElementById('form_button_area');
+  var resoMenu = document.getElementById('set_reso');
+  removeResoMenu.removeChild(resoMenu);
+
+}
+
+function resoSet() {
+  resolution = document.getElementById('set_reso').value;
+  return resolution = resolution
 }
