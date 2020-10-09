30e06cc673def6a51afa430a23aac91af223c56e
rascasse-monaco <mktwkhr@gmail.com>
2020-10-09 17:40:54 +0900

    ニコニコ動画対応

--- add-form.js
+++ add-form.js
@@ -4,6 +4,7 @@ let urlNum = 1 ;//動画URLのナンバー
 let resoArray = new Array();
 let globalInputUrl = null;
 let iframe = null;
+let nicoScript = null;
 
 //動画埋込ボタン押下後処理用関数
 function embed(){
@@ -38,39 +39,61 @@ function embed(){
 
 
 //iframeを作成
+//埋め込みエリアに動画（iframe）を埋め込み
 function createIframe() {
+//YouTube PC スマホ短縮URL用分岐、ニコニコ動画分岐
+  let input_url = document.getElementById("input_url-" + urlNum).value;
+  let youTube_url = /youtube/ig;
+  let niconico_url = /nicovideo/ig;
+  if (youTube_url.test(input_url)) {//youtubeの文字列があったらtrue
+    globalInputUrl = input_url.split('v=')[1];//通常URL用ID抽出
+    youTubeIframe();//YouTube用Iframe作成関数
+  } else if (niconico_url.test(input_url)) {//nocovideoの文字列があったらtrue
+    globalInputUrl = input_url.split('watch/')[1];//ニコニコ動画URL用ID抽出
+    nicoVideoScriptGen();
+  } else {
+    globalInputUrl = input_url.split('be/')[1];//短縮URL用ID抽出
+    youTubeIframe();//YouTube用Iframe作成関数
+  }
 
-//スマホ短縮URL用分岐
-let input_url = document.getElementById("input_url-" + urlNum).value;
-let youTube_url = /youtube/ig;
-let niconico_url = /nicovideo/ig;
-if (youTube_url.test(input_url)) {
-  globalInputUrl = input_url.split('v=')[1];//通常URL用
-  youTubeIframe();//YouTube用Iframe作成関数
-} else {
-  globalInputUrl = input_url.split('be/')[1];//短縮URL用
-  youTubeIframe();//YouTube用Iframe作成関数
 }
 
-  //埋め込みエリアに動画（iframe）を埋め込み
-  let output_url = document.getElementById("output_url-" + urlNum);
-      output_url.innerHTML = iframe;
-}
-
-//YouTube用Iframe作成関数
+//YouTube用 Iframe作成関数
 function youTubeIframe() {
   /**iframeに動画のIDとサイズ等を入れ込んで代入
    * ?enablejsapi=1によって"https://www.youtube.com/iframe_api"を使用可能にして、
    * apiオプションを追加できるようにする。(できてない)
   */
-  iframe = '<iframe id="player" width= ' + resoArray[0] + ' height=' +  resoArray[1] + ' src=" https://www.youtube.com/embed/' + globalInputUrl + '?rel=0&  amp;enablejsapi=1&amp;widgetid=1' + '" frameborder="0"    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;   picture-in-picture" allowfullscreen></iframe>';
+  iframe = 
+    '<iframe id="player" width= ' + 
+    resoArray[0] + 
+    ' height=' + 
+    resoArray[1] + 
+    ' src=" https://www.youtube.com/embed/' + 
+    globalInputUrl + 
+    '?rel=0&  amp;enablejsapi=1&amp;widgetid=1' + 
+    '" frameborder="0"    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;   picture-in-picture"  allowfullscreen></iframe>';
+
+  let output_url = document.getElementById("output_url-" + urlNum);
+  output_url.innerHTML = iframe;
 } 
 
-/**
+//ニコニコ動画用 埋込scriptリンク作成関数
 function nicoVideoScriptGen() {
-  <script src="https://embed.nicovideo.jp/watch/sm36122188/script?w=480&h=270"></script>
+  //iframeに動画のIDとサイズ等を入れ込んで代入
+  nicoScript =
+    '<iframe allowfullscreen="allowfullscreen" allow="autoplay" frameborder="0" width="' +
+    resoArray[0] + 
+    '" height="' + 
+    resoArray[1] +
+    '" src="' +  
+    'https://embed.nicovideo.jp/watch/' +
+    globalInputUrl +
+    '?oldScript=1&amp;referer=&amp;from=0&amp;allowProgrammaticFullScreen=1" style="max-width: 100%;"></iframe>'
+    
+  let output_url = document.getElementById("output_url-" + urlNum);
+  output_url.innerHTML = nicoScript;
 }
-*/
 
 //YouTubeApi読み込みとオプション設定部分
 var tag = document.createElement('script');
@@ -92,10 +115,7 @@ function onPlayerReady(event) {
   event.target.playVideo();
   event.target.mute();
 }
-function onPlayerStateChange(event) {
-
-}
-
+function onPlayerStateChange(event) {}
 
 //埋め込み動作によって不要になる要素を削除する
 function remove() {
@@ -103,21 +123,17 @@ function remove() {
     const removeFormArea = document.getElementById('form_button_area');
     let form = document.getElementById('input_url-' + urlNum);
     removeFormArea.removeChild(form);
-  
-  
     //埋め込みボタン削除
     const removeEmbedBotton = document.getElementById('form_button_area');
     const embedButton = document.getElementById('embed_button');
     removeEmbedBotton.removeChild(embedButton);
-  
-    //動画サイズプルダウンメニュー削除（初回埋め込み移行エラー出るので要修正）
+    //動画サイズプルダウンメニュー削除
     const removeResoMenu = document.getElementById('form_button_area');
     const resoMenu = document.getElementById('set_reso');
     if (resoMenu) {
       removeResoMenu.removeChild(resoMenu);
     }
 }
-
 
 //セレクトメニューの動画の大きさの選択肢から縦横サイズの配列を代入する
 function resoSet() {
@@ -141,7 +157,7 @@ function addForm() {
     input_data.type = 'text';
     input_data.setAttribute("class", "text_area");
     input_data.id = 'input_url-' + (urlNum + 1);
-    input_data.placeholder = '追加するYouTube動画のURLをペースト';
+    input_data.placeholder = '追加する動画のURLをペースト';
   
   //フォームエリア作成
   const parentFormarea = document.getElementById('form_button_area');
