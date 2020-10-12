f83378931206af56ba94af0660521d804352cee9
rascasse-monaco <mktwkhr@gmail.com>
2020-10-12 20:04:19 +0900

    ファビコン変更、画面サイズ取得方法変更

--- add-form.js
+++ add-form.js
@@ -174,7 +174,7 @@ function resoSet() {
       } else if (resolution === 'large') {
         resoArray = ['800', '450'];
       } else {
-        let width = window.outerWidth-20;
+        let width = window.innerWidth-20;
         let hight = parseInt(width * 0.5625, 10);
         resoArray = [ `'${width}'`, `'${hight}'`];
       }
